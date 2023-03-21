59af2280556ddcad300a478ad5191d418e546362
kyh <kyh_0625@naver.com>
2023-03-21 20:12:20 +0900

    react func class ver3

--- react_func_class/src/App.js
+++ react_func_class/src/App.js
@@ -1,50 +1,59 @@
 import './App.css';
-import React from 'react';
-import { useState } from 'react';
+import React, {useState} from 'react';
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello world</h1>
-      <FuncComp initNumber={'0부터 1사이의 랜덤한 숫자가 나옵니다'} />
-      <ClassComp initNumber={'0부터 1사이의 랜덤한 숫자가 나옵니다'} />
+      <h1>hello world</h1>
+        <FuncComp initNumber={'랜덤숫자 만들기'}></FuncComp>
+        <ClassComp initNumber={'랜덤숫자 만들기'}></ClassComp>
     </div>
   );
 }
 
 function FuncComp(props){
-  const [number, setNumber] = useState(props.initNumber);
-  const [date, setDate] = useState((new Date().toString()));
+  const [number,setNumber] = useState(props.initNumber);
+  const [_date, setDate] = useState((new Date()).toString());
   return (
     <div className="container">
       <h2>function style component</h2>
-      <p>Number : {number}</p>
-      <p>Date : {date}</p>
+      <p>Number: {number}</p>
+      <p>Date: {_date}</p>
       <input type="button" value="random" onClick={()=>{
         setNumber(Math.random());
+      }}></input>
+      <input type="button" value="date" onClick={()=>{
         setDate((new Date()).toString());
       }} />
     </div>
   );
 }
-
+        
+var classStyle ='color:red';
 class ClassComp extends React.Component{
+  componentWillMount(){
+    console.log('%cclass=>componentWillMount',classStyle);
+  }
+  componentDidMount(){
+    console.log('%cclass=>componentDidMount',classStyle);
+  }
   state ={
     number:this.props.initNumber,
-    date: new Date().toString()
+    __date:(new Date()).toString()
   }
   render(){
+    console.log('%cclass=>render',classStyle);
     return(
       <div className="container">
         <h2>class style component</h2>
-        <p>Number : {this.state.number} </p>
-        <p>Date : {this.state.date}</p>
+        <p>Number: {this.state.number}</p>
+        <p>Date: {this.state.__date}</p>
         <input type="button" value="random" onClick={function(){
           this.setState({number:Math.random()});
-          this.setState({date:new Date().toString()});
-
-          }.bind(this)
-        } />
+        }.bind(this)} />
+        <input type="button" value="date" onClick={function(){
+          this.setState({__date:new Date().toString()});
+        }.bind(this)} />
       </div>
     );
   }
