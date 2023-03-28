c61a784f2ef4d599eb4c62ceefaa0f014f8b04c8
kyh <kyh_0625@naver.com>
2023-03-29 01:52:42 +0900

    before just creating

--- node.js/tmp.js
+++ node.js/tmp.js
@@ -2,67 +2,80 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
+function templateHTML(title, list, data){
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>${title}</title>
+    </head>
+    <body>
+        <h1><a href="/">WEB</a></h1>
+        <ol>
+            ${list}
+        </ol>
+        <a href="/create">create</a>
+        <h2>${title}</h2>
+        <p>${data}</p>
+    </body>
+    </html>
+    `;
+}
 
+function templateList(filelist){
+    let list ='';
+    filelist.forEach(element => {
+        list +=`<li><a href="/?id=${element}">${element}</a></li>`;
+    });
+    return list;    
+}
 
 const app = http.createServer(function(request, response){
     let queryData = url.parse(request.url,true).query;
-    console.log(url.parse(request.url,true).query.id);
+    console.log(url.parse(request.url,true));
     const pathname = url.parse(request.url,true).pathname;
+    // console.log(pathname);
     if(pathname === '/'){
         if(queryData.id === undefined){
+            fs.readdir('./data',function(error,filelist){
+                const list = templateList(filelist);
                 let title = 'welcome';
                 let data = 'node.js';
-                const template = `
-                <!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <title>${title}</title>
-                </head>
-                <body>
-                    <h1><a href="/">WEB</a></h1>
-                    <ol>
-                        <li><a href="/?id=html">html</a></li>
-                        <li><a href="/?id=css">css</a></li>
-                        <li><a href="/?id=javascript">javascript</a></li>
-                    </ol>
-                    <h2>${title}</h2>
-                    <p>${data}</p>
-                </body>
-                </html>
-                `;
-                response.writeHead(200);
-                response.end(template);
-        } else {
-            fs.readFile(`./data/${queryData.id}`,'utf8',function(error,data){
-                let title = queryData.id;
-                const template = `
-                <!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <title>${title}</title>
-                </head>
-                <body>
-                    <h1><a href="/">WEB</a></h1>
-                    <ol>
-                        <li><a href="/?id=html">html</a></li>
-                        <li><a href="/?id=css">css</a></li>
-                        <li><a href="/?id=javascript">javascript</a></li>
-                    </ol>
-                    <h2>${title}</h2>
-                    <p>${data}</p>
-                </body>
-                </html>
-                `;
+                const template = templateHTML(title,list,data);
                 response.writeHead(200);
                 response.end(template);
             });
+        } else {
+            fs.readdir('./data',function(error,filelist){
+                fs.readFile(`./data/${queryData.id}`,'utf8',function(error,data){
+                    const list = templateList(filelist);
+                    let title = queryData.id;
+                    const template = templateHTML(title,list,data);
+                    response.writeHead(200);
+                    response.end(template);
+                });
+            });
         }
+    } else if(pathname === '/create'){
+        fs.readdir('./data',function(error,filelist){
+            const list = templateList(filelist);
+            let title = 'CREATE';
+            let data = `
+            <form action="craete_process" method="post">
+            <p><input type="text" name="title" placeholder="title" /></p>
+            <p><textarea name="description" placeholder="description"></textarea></p>
+            <p><input type="submit" value="create" /></p>
+            </form>
+            `;
+            const template = templateHTML(title,list,data);
+            response.writeHead(200);
+            response.end(template);
+        });
+    } else if(pathname === '/create_process'){
+        
     } else {
             response.writeHead(404);
             response.end('notFound');
